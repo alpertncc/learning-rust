c72e54675969a8802b18411a48902cfa44081b59
BlockofChain <87220239+alpertncc@users.noreply.github.com>
2022-12-30 15:44:46 +0300

    Delete Chapter1 directory

--- Chapter1/hello_cargo/target/debug/deps/hello_cargo-a66cbd52cc0feedc.d
+++
@@ -1,5 +0,0 @@
-C:\Users\alper\Documents\learning-rust\Chapter1\hello_cargo\target\debug\deps\hello_cargo-a66cbd52cc0feedc.rmeta: src\main.rs
-
-C:\Users\alper\Documents\learning-rust\Chapter1\hello_cargo\target\debug\deps\hello_cargo-a66cbd52cc0feedc.d: src\main.rs
-
-src\main.rs:
--- Chapter1/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-C:\Users\alper\Documents\learning-rust\Chapter1\hello_cargo\target\debug\hello_cargo.exe: C:\Users\alper\Documents\learning-rust\Chapter1\hello_cargo\src\main.rs
