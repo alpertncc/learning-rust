c2ccf5e799d587f8018ba200f4111092f3ad6974
alpertncc <alperen.tunc.02@gmail.com>
2022-12-30 15:37:10 +0300

    Learned how to use basic cargo commands

---
+++ Chapter1/hello_cargo/target/debug/deps/hello_cargo-a66cbd52cc0feedc.d
@@ -0,0 +1,5 @@
+C:\Users\alper\Documents\learning-rust\Chapter1\hello_cargo\target\debug\deps\hello_cargo-a66cbd52cc0feedc.rmeta: src\main.rs
+
+C:\Users\alper\Documents\learning-rust\Chapter1\hello_cargo\target\debug\deps\hello_cargo-a66cbd52cc0feedc.d: src\main.rs
+
+src\main.rs:
