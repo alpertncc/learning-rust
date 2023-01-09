2cdb25b97ddde96ac4bebbab39280969a1ef52f6
alpertncc <alperen.tunc.02@gmail.com>
2023-01-09 23:15:54 +0300

    Started to Chapter 2

---
+++ chapter2/variables/src/main.rs
@@ -0,0 +1,19 @@
+fn main () {
+   /*
+    let x = 5; 
+    println!("The value of x is: {x}");
+
+    x = 6;
+    println!("The value of x is: {x}");
+
+    /* X is immutable variable, hence you will take an error. */
+    */
+
+
+    let mut x = 5;  /* mut will make your variable mutable. */
+    println!("The value of x is: {x}");
+
+    x = 6;
+    println!("The value of x is: {x}");
+
+}
