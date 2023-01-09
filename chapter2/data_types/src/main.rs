0dc2772e359035ad73124723dcbc7929fab00471
alpertncc <alperen.tunc.02@gmail.com>
2023-01-10 00:02:43 +0300

    Data Types

---
+++ chapter2/data_types/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    /* Two data type subsets we have : 
+            1-> Scalar
+            2-> Compound
+        Rust is a statically typed language, it must know the types of all variables.
+    */
+
+    let guess: u32 = "42".parse().expect("Not a number!"); /* Convert from String to Numaric Type */
+    println!("{guess}");
+
+    /* SCALAR TYPES 
+            -> Represent a single value,
+            -> 4 primary scalar types:
+                --> Integers
+                --> Floating-point numbers
+                --> Booleans
+                --> Characters
+    */
+
+    /* -> Integer Type 
+            i -> signed
+            u -> unsigned
+            8/16/32/64/128 bits & arch ( isize / usize )    
+    */
+        
+
+
+
+}
